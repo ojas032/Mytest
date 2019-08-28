c2ce3b44eb0b8223cbb561ce9a3abd96e855a2f8
ojas032 <corpojasltd@gmail.com>
2019-08-28 12:58:46 +0530

    message

--- fst.py
+++ fst.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import Flask,redirect,url_for
+from flask import Flask, redirect, url_for, request,render_template
 app = Flask(__name__)
 
-@app.route('/adminn')
-def hello_admin():
-   return 'Hello Admin'
+@app.route('/')
+def start():
+	return render_template('index.html')
 
-@app.route('/guest/<guest>')
-def hello_guest(guest):
-   return 'Hello %s as Guest' % guest
+@app.route('/success/<name>/<word>')
+def success(name,word):
+   return 'welcome %s %s'%(name,word)
 
-@app.route('/user/<name>')
-def hello_user(name):
-   if name =='admin':
-      return redirect(url_for('hello_admin'))
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      password=request.form['pass']
+      return redirect(url_for('success',name = user,word=password))
+
    else:
-      return redirect(url_for('hello_guest',guest = name))
-
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
 
 if __name__ == '__main__':
-	app.run(debug = True)
+   app.run(debug = True)
